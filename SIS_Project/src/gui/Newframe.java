592b24028c041a16a6194faab29f4942ea24ad06
Eyad scorpion <eyadgaafar5@gmail.com>
2026-03-08 06:07:29 +0200

    Update Newframe.java

--- SIS_Project/src/gui/Newframe.java
+++ SIS_Project/src/gui/Newframe.java
@@ -6,7 +6,7 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
-public class New extends JFrame {
+public class Newframe extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
@@ -18,7 +18,7 @@ public class New extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					New frame = new New();
+					Newframe frame = new Newframe();
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -30,7 +30,7 @@ public class New extends JFrame {
 	/**
 	 * Create the frame.
 	 */
-	public New() {
+	public Newframe() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
